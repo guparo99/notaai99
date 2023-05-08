ce656f403537211aeba482e3cc5d0ad83bca2010
guparo99 <guparo99@gmail.com>
2023-05-07 23:38:15 -0400

    07 commit

--- noteai/chat.py
+++ noteai/chat.py
@@ -1,12 +1,1 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',  # O la dirección IP del servidor PostgreSQL
-        'PORT': '5432',  # El puerto por defecto de PostgreSQL
-        'NAME': 'noteai',
-        'USER': 'postgres',
-        'PASSWORD': 'aa090895',
-    }
-}
-
-como conecto a la base de datos noteai creada en pgAdmin
+vacio
